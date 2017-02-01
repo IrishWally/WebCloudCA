836d44815c89fd2b1637d001bee6a6866666d740
IrishWally <10345563@mydbs.ie>
2017-02-01 17:01:24 +0000

    Add files via upload

---
+++ WebCloudCA/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCloudCA.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        //Controller for the Reviews Page
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+    }
+}
